dc6d0b935916373eac9fc71ce42e4be9c3084297
Kayla J. Kane <33146347+KaylaJKLab@users.noreply.github.com>
2022-08-13 12:29:52 +0200

    Create kjk_streak_colors.php

---
+++ src/kjk_streak_colors.php
@@ -0,0 +1,12 @@
+[
+  "background" => "#383838",
+  "border" => "#e4e2e2",
+  "stroke" => "#e4e2e2",
+  "ring" => "#F40C96",
+  "fire" => "#F40C96",
+  "currStreakNum" => "#03EDFF",
+  "sideNums" => "#F40C96",
+  "currStreakLabel" => "#03EDFF",
+  "sideLabels" => "#F40C96",
+  "dates" => "#7F79C9",
+]
